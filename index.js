73fb9d8f4a9a969d38ad1b956ee2159e1cda4bc6
Sahil Gupta <guptas3067@gmail.com>
2025-09-22 13:01:46 +0530

    new domain cors

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const io = require("socket.io")(8900, {
     cors: {
-        origin: ["http://localhost:3000", "https://beyinc-frontend.onrender.com", "https://www.beyinc.org", "https://beyinc-frontend.vercel.app", "https://yellow-mushroom-0aec0e610.2.azurestaticapps.net"],
+        origin: ["http://localhost:3000", "https://beyinc-frontend.onrender.com", "https://www.beyinc.org", "https://beyinc-frontend.vercel.app", "https://yellow-mushroom-0aec0e610.2.azurestaticapps.net",'https://www.bloomr.world'],
     },
 });
 
